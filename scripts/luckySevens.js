606247e997f6f75bf883de5e9776eab61eaa211d
Mitch Watson <Wats0314@d.umn.edu>
2020-01-06 18:30:17 -0600

    FunctionDoneNeedCSS

--- scripts/luckySevens.js
+++ scripts/luckySevens.js
@@ -1,43 +1,49 @@
 
 function playGame() {
-var startingbet = document.getElementById("betamount").value;
-document.getElementById("moneyinput").innerHTML = startingbet;
-console.log(startingbet)
+var startingBet = document.getElementById("betAmount").value;
+console.log(startingBet)
 
-
-function rollTwo(){
+function rollTwo(){ 
 return Math.floor(Math.random() * 6 + 1) + Math.floor(Math.random() * 6 + 1); 
 }
 
+var betMoney = parseInt(startingBet, 10);
 	
-if (startingbet <= 0){
+if (betMoney <= 0){
 alert("Enter a $ amount that is more than 0")}	
 
 	
 var moneyLog = new Array();
 	
-	for(var rollCount = 0; startingbet > 0; ++ rollCount){
+	for(var rollCount = 0; betMoney > 0; ++ rollCount){
 	var diceSum = rollTwo();
-	moneyLog.push(startingbet);
-	console.log(startingbet);
-		if(diceSum !== 7){ startingbet -= 1}
-		else{startingbet += 4}
+	moneyLog.push(betMoney);
+	console.log(betMoney);
+		if(diceSum !== 7){ betMoney -= 1}
+		else{betMoney += 4}
 }
 
-var highestMoney = (Math.max(...moneyLog))
+function getMax(){ return Math.max(...moneyLog)}
 
+var highestMoney = getMax();
 	
 for (i = 0; i < moneyLog.length; i++){
 		if (moneyLog[i] == highestMoney){var highRollIndex = i}
-else {}
+	
+
 }
-			
+document.getElementById("highMoney").innerHTML = getMax();
+document.getElementById("initialBet").innerHTML = startingBet;
+document.getElementById("totalRolls").innerHTML = rollCount;
+document.getElementById("rollatHighest").innerHTML = highRollIndex;
+		
 console.log("at roll " + highRollIndex + " we were rich")		
 console.log(Math.max(...moneyLog) + " was the most money we had")	
 console.log(rollCount + " rolls before going broke")
 
-	
-}	
+}
+
+
 
 
 
